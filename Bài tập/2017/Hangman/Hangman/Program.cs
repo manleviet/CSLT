54dcbcc24d83cb08595b60092e0fb531122652f6
Lê Viết Mẫn <lvman@hce.edu.vn>
2017-11-07 12:55:38 +0700

    Bổ sung thư viện cho Bài tập 2017
    Bổ sung thư viện vẽ hình Hangman

---
+++ Bài tập/2017/Hangman/Hangman/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HangmanImage.PrintImage(0); Console.WriteLine();
+            HangmanImage.PrintImage(6);
+            Console.ReadKey();
+        }
+    }
+}
